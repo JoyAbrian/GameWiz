9db5d3b3b722596f3c085e911ae8ffbbb8a190dc
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-05-23 22:21:13 +0800

    feat : create platform class

---
+++ app/src/main/java/com/ruukaze/gamewiz/Class/Platform.java
@@ -0,0 +1,37 @@
+package com.ruukaze.gamewiz.Class;
+
+public class Platform {
+    private int id;
+    private String name;
+    private int platform_logo;
+
+    public Platform(int id, String name, int platform_logo) {
+        this.id = id;
+        this.name = name;
+        this.platform_logo = platform_logo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPlatform_logo() {
+        return platform_logo;
+    }
+
+    public void setPlatform_logo(int platform_logo) {
+        this.platform_logo = platform_logo;
+    }
+}
