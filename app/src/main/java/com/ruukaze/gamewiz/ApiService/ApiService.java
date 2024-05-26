5c2173430fd53673a3e341343a6c6ada7e3b8593
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-05-26 13:00:53 +0800

    feat: add more API services

--- app/src/main/java/com/ruukaze/gamewiz/ApiService/ApiService.java
+++ app/src/main/java/com/ruukaze/gamewiz/ApiService/ApiService.java
@@ -1,14 +1,42 @@
 package com.ruukaze.gamewiz.ApiService;
 
+import com.ruukaze.gamewiz.Class.Company;
+import com.ruukaze.gamewiz.Class.Game;
+import com.ruukaze.gamewiz.Class.Genre;
+import com.ruukaze.gamewiz.Class.Platform;
+
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
 
 public interface ApiService {
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: text/plain"
+    })
+    @GET("/games")
+    Call<List<Game>> getGames();
 
-//    @Headers({
-//            "Accept: application/json",
-//            "Content-Type: text/plain"
-//    })
-//    @GET("/games")
-//    Call<List<Game>> getGames();
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: text/plain"
+    })
+    @GET("/companies")
+    Call<List<Company>> getCompanies();
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: text/plain"
+    })
+    @GET("/genres")
+    Call<List<Genre>> getGenres();
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: text/plain"
+    })
+    @GET("/platforms")
+    Call<List<Platform>> getPlatforms();
 }
